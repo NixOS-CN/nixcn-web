9bb89b31cf14367d20f810c07214b711356108b5
Noa Virellia <noa@requiem.garden>
2025-12-11 19:47:03 +0800

    feat(docs/souvenir-customization): improvement
    - Added more info about badge/nameplate customization.
    - Implemented form for souvenir customization requests.
    
    Signed-off-by: Noa Virellia <noa@requiem.garden>

--- astro.config.mjs
+++ astro.config.mjs
@@ -32,7 +32,7 @@ export default defineConfig({
                     href: 'https://github.com/NixOS-CN/nixcn-web',
                 },
             ],
-            sidebar: ['meetup-2-guide', 'badge-customization'],
+            sidebar: ['meetup-2-guide', 'souvenir-customization'],
             plugins: [
                 starlightUITweaks({
                     navbarLinks: [
